eed2c6151cb56b51e59206f4a458e0d1aa5acb56
MOHAMMAD SALIM SOARES SILVA <24122313@CFP127ALUNO.LOCAL>
2025-02-25 14:36:07 -0300

    Adição do farol no React

--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -1,5 +1,5 @@
 import { useSearchParams, useNavigate } from 'react-router-dom'
-import Lighthouse from '../../components/farol'
+import Lighthouse from '../../components/Lighthouse/farol'
 import '../../styles/cssformatting.css'
 import './homePage.css'
 
