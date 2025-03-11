eebc4eb0f1a41ee1c16242ab585e584cafced8b6
Mohammad Salim.S.S <77904933+moh4b-z@users.noreply.github.com>
2025-03-11 17:14:16 -0300

    Update homePage.jsx

--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -1,5 +1,5 @@
 import { useSearchParams, useNavigate } from 'react-router-dom'
-import Lighthouse from '../../components/Lighthouse/farol'
+import Lighthouse from '../../components/Lighthouse/lightHouse'
 import '../../styles/cssformatting.css'
 import './homePage.css'
 
